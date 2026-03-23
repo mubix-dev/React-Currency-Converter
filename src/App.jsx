40539ba3b66ae55b9a0b9539fea69fe31623d4c2
Mubeen Khan <mubix.dev@gmail.com>
2026-03-23 23:57:14 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='w-full min-h-screen bg-gray-800 text-white flex justify-center items-center'>
       <div className='w-2/5 h-52 bg-gray-600'>
-          
+          <div></div>
       </div>
     </div>
   )
