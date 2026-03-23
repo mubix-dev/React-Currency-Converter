b5e795479bc75a8590c35e3b492f4760c8a105fc
Mubeen Khan <mubix.dev@gmail.com>
2026-03-23 23:54:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full min-h-screen bg-gray-800 text-white flex justify-center items-center'>
+      <div className='w-2/5 h-52 bg-gray-600'>
+          
+      </div>
+    </div>
+  )
+}
+
+export default App
